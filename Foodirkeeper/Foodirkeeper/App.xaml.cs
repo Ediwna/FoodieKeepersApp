f2048844f0a054bb0c9dc932317a4cd8681cfade
Pacôme MANCEAUX <pacome.manceaux@edu.devinci.fr>
2021-12-03 18:45:19 +0900

    Ajoutez des fichiers projet.

---
+++ Foodirkeeper/Foodirkeeper/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Foodirkeeper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage()); // 创建根页面
+            //MainPage = new Page1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Foodirkeeper/Foodirkeeper/Views/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Foodirkeeper
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked1(object sender, EventArgs e)
+        {
+            //button1.Text = "Success";
+            Button b = sender as Button;
+            b.Text = "Success";
+        }
+
+        private async void Button_Clicked2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+            
+        }
+
+    }
+}
